aaa446066a8c04b69523b103c1a4b43f3532ed06
Samet ALTUNDAG <smtaltundag63@gmail.com>
2023-12-28 14:28:47 +0300

    Basic pages created.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\PageHomeController;
+use App\Http\Controllers\Frontend\PageController;
+use App\Http\Controllers\Frontend\PageHomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,4 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/',[PageHomeController::class, 'anasayfa'])->name('anasayfa');
+Route::get('/', [PageHomeController::class, 'anasayfa'])->name('anasayfa');
+Route::get('/hakkimizda', [PageController::class, 'hakkimizda'])->name('hakkimizda');
+Route::get('/urunler', [PageController::class, 'urunler'])->name('urunler');
+Route::get('/urun/detay', [PageController::class, 'urundetay'])->name('urundetay');
+Route::get('/iletisim', [PageController::class, 'iletisim'])->name('iletisim');
+Route::get('/sepet', [PageController::class, 'sepet'])->name('sepet');
